7922581a58bec45c1a368f7c24e3b65eba79d8e9
Hesham Hussen <33562928+elmodeer@users.noreply.github.com>
2021-04-08 15:16:41 +0200

    Add files via upload

---
+++ sendEmail.py
@@ -0,0 +1,26 @@
+import smtplib, ssl
+from email.mime.multipart  import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def send_to_me(message, title): 
+    port = 465  # For SSL
+    password = 'tlsBookDev'
+    sender = 'tlsbookingdev@gmail.com'
+    
+    # TODO !! INSERT YOUR EMAIL 
+    me = ['something at gmail.com']
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login("tlsbookingdev@gmail.com", password)
+        
+        msg = MIMEMultipart()
+        msg['From'] = sender
+        msg['To'] = ', '.join(me)
+        msg['Subject'] = title
+        msg.attach(MIMEText(message))
+
+        server.sendmail(sender, me, msg.as_string())    
+        server.quit()
