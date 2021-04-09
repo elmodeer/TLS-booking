5edd25b48889bd148dda5071cbca42532657d210
Ismail Cherri <ismailcherri@gmail.com>
2021-04-09 10:20:06 +0200

    Adds support for auto-detecting current OS and system

--- sendEmail.py
+++ sendEmail.py
@@ -1,13 +1,14 @@
-import smtplib, ssl
-from email.mime.multipart  import MIMEMultipart
+import smtplib
+import ssl
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
-def send_to_me(message, title): 
+def send_to_me(message, title):
     port = 465  # For SSL
     password = 'tlsBookDev'
     sender = 'tlsbookingdev@gmail.com'
-    
+
     # TODO !! INSERT YOUR EMAIL(S)
     me = ['something at gmail.com', 'something else @ gmail.com']
     # Create a secure SSL context
@@ -15,12 +16,12 @@ def send_to_me(message, title):
 
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login("tlsbookingdev@gmail.com", password)
-        
+
         msg = MIMEMultipart()
         msg['From'] = sender
         msg['To'] = ', '.join(me)
         msg['Subject'] = title
         msg.attach(MIMEText(message))
 
-        server.sendmail(sender, me, msg.as_string())    
+        server.sendmail(sender, me, msg.as_string())
         server.quit()
--- tls-book.py
+++ tls-book.py
@@ -1,20 +1,19 @@
-import time
 import datetime
+import os
+import platform
+
+import time
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
+
 from sendEmail import send_to_me
-from bs4 import BeautifulSoup
 
-def login(driver): 
 
+def login(driver):
     if 'myapp' in driver.current_url:
         print('refresh')
-        driver.refresh() 
+        driver.refresh()
         return True
     else:
         driver.get('https://de-legalization.tlscontact.com/eg/CAI/login.php')
@@ -30,48 +29,46 @@ def login(driver):
 
         driver.find_element_by_class_name("submit").click()
 
-        delay = 5 # seconds
+        delay = 5  # seconds
         time.sleep(delay)
 
         if 'myapp' in driver.current_url:
             print('Login Successful')
             return True
-        
+
         else:
             print('Login Unuccessful !!')
             return False
 
 
-
 def is_an_appointment_free(driver):
-    table = driver.find_element_by_id('timeTable')   
-    tableContent = BeautifulSoup(table.get_attribute('innerHTML'), features="html.parser")
+    table = driver.find_element_by_id('timeTable')
+    table_content = BeautifulSoup(table.get_attribute('innerHTML'), features="html.parser")
 
-    days = tableContent.find_all("span", {"class": "appt-table-d"})
+    days = table_content.find_all("a", {"class": "appt-table-d"})
     other_day = False
     str_days = str(days)
 
     # TODO edit as the days u see now!! 
     if '16' not in str_days or '17' not in str_days:
         print('day')
-        other_day = True   
+        other_day = True
 
-    # TODO edit as the slots u see now!! 
-    appointments = tableContent.find_all("a")
+    # TODO edit as the slots u see now!!
+    appointments = table_content.find_all("a")
     full = True
-    for appointment in appointments: 
+    for appointment in appointments:
         str_appointment = str(appointment)
         if 'full' not in str_appointment:
-            full = False  
-    
-    # TODO edit!! 
+            full = False
+
+    # TODO edit!!
     if not full or other_day or len(appointments) != 14 or len(days) != 2:
         print('Appointment found')
         return True
-    else: 
+    else:
         print('No found')
         return False
-
 
 
 def check(driver):
@@ -81,21 +78,40 @@ def check(driver):
             message = 'Luke, Iam your father ... U have to go there now => https://de-legalization.tlscontact.com/eg/CAI/login.php'
             send_to_me(message, title)
 
-    except Exception as e: 
-        title = 'Somnthing went wrong'
+    except Exception as e:
+        title = 'Something went wrong'
         message = str(e)
         print(message)
         # send_to_me(message, title)
 
 
+def getOS():
+    uname = platform.uname()
+    driver_name = 'geckodriver'
+    if uname.system == 'Darwin':
+        driver_name += '_macos'
+
+    if uname.system == 'Linux':
+        driver_name += '_linux'
+
+    if uname.system == 'Windows':
+        driver_name += '_win'
+
+        if uname.machine == 'x86_64':
+            driver_name += '64.exe'
+        else:
+            driver_name += '32.exe'
+
+    return driver_name
+
 
 firefoxOptions = Options()
 firefoxOptions.add_argument("-headless")
-driver = webdriver.Firefox(executable_path="./drivers/geckodriver", options=firefoxOptions)
-
+currentDirectory = os.getcwd()
+driver = webdriver.Firefox(executable_path=currentDirectory + '/drivers/' + getOS(), options=firefoxOptions)
+check(driver)
 while 1:
     print(datetime.datetime.now())
     check(driver)
     print(' -------------------------------------------------------- ')
     time.sleep(5 * 60)
-
